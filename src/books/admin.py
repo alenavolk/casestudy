846947068c950de45c3c5b7fba758196389cf24c
Alena Volkova <volkova.ag@gmail.com>
2024-03-26 23:38:18 -0400

    Add models and fixtures, define admin UI

--- src/books/admin.py
+++ src/books/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, Language, Book
+
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'country', 'language', 'view_year', 'pages']
+
+    @admin.display(description='year')
+    def view_year(self, obj):
+        return f"{abs(obj.year)} B.C." if obj.year < 0 else obj.year
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Book, BookAdmin)
--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=128, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    year = models.SmallIntegerField()
+    pages = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.title
